5a577583e6a7966507c0cc2b8b7d384bd96cffb7
Vance Ingalls <vance@heygen.com>
2026-07-01 12:14:24 -0700

    feat(core): emit paused GSAP timeline script from figma motion spec

--- packages/core/src/figma/index.ts
+++ packages/core/src/figma/index.ts
@@ -1,2 +1,22 @@
-export * from "./types";
-export { MAX_FREEZE_BYTES } from "./freeze";
+export { parseFigmaRef } from "./parseFigmaRef";
+export {
+  MAX_FREEZE_BYTES,
+  exceedsFreezeCap,
+  freezeBytes,
+  freezeUrl,
+  freezeLocalFile,
+} from "./freeze";
+export {
+  mediaDir,
+  manifestPath,
+  typeDirPath,
+  isFigmaManifestRecord,
+  readManifest,
+  appendRecord,
+  findByFigmaNode,
+  nextId,
+} from "./manifest";
+export { buildAssetSnippet } from "./assetSnippet";
+export { mapEase } from "./motionEase";
+export { motionToGsap } from "./motionToGsap";
+export { emitTimelineScript } from "./emitTimelineScript";
